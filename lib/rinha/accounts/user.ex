7c2041d1328e13b7a2e829db36dbd1b8dd9260ca
Anderson Konzen <anderson@anderkonzen.com>
2023-08-24 22:06:15 -0300

    Add credo

--- lib/rinha/accounts/user.ex
+++ lib/rinha/accounts/user.ex
@@ -1,4 +1,6 @@
 defmodule Rinha.Accounts.User do
+  @moduledoc false
+
   use Ecto.Schema
   import Ecto.Changeset
 
--- lib/rinha_web/telemetry.ex
+++ lib/rinha_web/telemetry.ex
@@ -1,4 +1,6 @@
 defmodule RinhaWeb.Telemetry do
+  @moduledoc false
+
   use Supervisor
   import Telemetry.Metrics
 
